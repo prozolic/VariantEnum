f2901a2060d775b1648f9374fdd4e2cf05e8bfd6
prozolic <42107886+prozolic@users.noreply.github.com>
2025-01-08 12:41:43 +0900

    first commit

---
+++ src/VariantEnum/Generator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace VariantEnum;
+
+[Generator(LanguageNames.CSharp)]
+public partial class Generator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+
+    }
+}
