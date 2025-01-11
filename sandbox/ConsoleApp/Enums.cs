a092c18fd1699ca0fc6f5c5faf583e727c17e1c5
prozolic <42107886+prozolic@users.noreply.github.com>
2025-01-12 00:10:19 +0900

    upd

--- sandbox/ConsoleApp/Enums.cs
+++ sandbox/ConsoleApp/Enums.cs
@@ -7,7 +7,21 @@ public enum IpAddrVariant : byte
     [VariantValueType(typeof(byte), typeof(byte), typeof(byte), typeof(byte))]
     V4,
     [VariantValueType(typeof(string))]
-    V6 = 3,
+    V6,
+    None
+}
 
-    None,
+[IgnoreVariant]
+public enum TestVariant
+{
+
+}
+
+public class A
+{
+    [IgnoreVariant]
+    public enum ABVariant
+    {
+
+    }
 }
